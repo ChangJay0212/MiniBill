06f921b0f0b653931d8e7bc04da4a5d7ea462645
ChangJay0212 <lastlife0212@gmail.com>
2025-07-24 13:12:19 +0800

    feat: add bean to use password hash

--- src/main/java/com/minibill/MiniBillApplication.java
+++ src/main/java/com/minibill/MiniBillApplication.java
@@ -2,11 +2,18 @@ package com.minibill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MiniBillApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiniBillApplication.class, args);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
